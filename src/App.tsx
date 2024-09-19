92080b015e7499de4e5594653c4aee2412457fa6
Bohdan Scherbak <bogmira777@gmail.com>
2024-09-19 14:58:01 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return <>
+    
+  </>;
+};
+
+export default App;
---
+++ src/includes/includes.tsx
@@ -0,0 +1,3 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
