95dc0a2abd45c38314f6c681ce35425d1a92cfb8
BogdanSch <bogmira777@gmail.com>
2024-09-27 23:10:02 +0200

    branch:main Added Header section, linked aos and bootstrap-icons to the project, removed unnecessary imports.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { FC } from "react";
 import Header from "./components/partials/Header";
 import Footer from "./components/partials/Footer";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const App: FC = () => {
+  AOS.init();
+
   return (
     <>
       <Header />
-      <main className="main">
-
-      </main>
+      <main className="main"></main>
       <Footer />
     </>
   );
--- src/includes/includes.tsx
+++ src/includes/includes.tsx
@@ -1,5 +1,7 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 import "/src/assets/css/main.css";
+// import "/src/assets/js/main.js";
